b9f6584b05d86f8e3f7d6368206e4040c9c066a3
Tulasi <tulsishuklag@gmail.com>
2026-07-13 09:52:07 +0530

    add backend/src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-2-x4kd.onrender.com/",
     credentials: true,
   },
 });
